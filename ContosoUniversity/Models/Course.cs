18a500ebbf1c0d2629a28519966a3118a4ad9a19
MizG <mizgoines@users.noreply.github.com>
2015-08-10 15:31:23 -0400

    MVC 5 Entity Framework 6 Lab first edit thru pg 33
    no changes

---
+++ ContosoUniversity/Models/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+
+//namespace ContosoUniversity.Models
+//{
+//    public class Course
+//    {
+//    }
+//}
